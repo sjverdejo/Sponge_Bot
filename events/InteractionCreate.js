9d739724b25783d7d5a1080ae13b3200af8f90d4
Sam Verdejo <sjverdejo@gmail.com>
2023-09-12 17:06:09 -0400

    Created slash command event handler and deploy script for slash commands

---
+++ events/InteractionCreate.js
@@ -0,0 +1,21 @@
+const { Events } = require('discord.js')
+
+module.exports = {
+  name: Events.InteractionCreate,
+  async execute(interaction) {
+    if (!interaction.isChatInputCommand()) return
+    
+    const command = interaction.client.commands.get(interaction.commandName)
+
+    if (!command) {
+      console.log(`No command matching ${interaction.commandName}`)
+      return
+    }
+
+    try {
+      await command.execute(interaction)
+    } catch (error) {
+      console.log(`Error exexcuting command ${interaction.commandName}, Error: `, error )
+    }
+  }
+}
